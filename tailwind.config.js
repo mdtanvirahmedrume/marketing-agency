d3ad55e51abdf103facb110b7f4c5dba1eb7c909
mdtanvirahmedrume <tstanvir371806@gmail.com>
2024-03-28 15:54:31 +0600

    added Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  //...
-  daisyui: {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+   daisyui: {
     themes: ["light", "dark", "cupcake"],
   },
 }
