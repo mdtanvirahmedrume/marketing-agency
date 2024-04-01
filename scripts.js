84352260298b24080d6a96281b2993132c189810
mdtanvirahmedrume <tstanvir371806@gmail.com>
2024-04-01 15:17:50 +0600

    design chnage

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
-let navclickButton = document.getElementById('navclick');
-let navCloseButton = document.getElementById('navClose');
-let dropdown = document.querySelector('.dropdown-content'); // Assuming your dropdown has a class 'dropdown-content'
+let navclickButton = document.getElementById("navclick");
+let navCloseButton = document.getElementById("navClose");
+let dropdown = document.querySelector(".dropdown-content"); // Assuming your dropdown has a class 'dropdown-content'
 
-navclickButton.addEventListener('click', () => {
-    // Hide navclickButton, Show navCloseButton
-    navclickButton.classList.add('hidden');
-    navCloseButton.classList.remove('hidden');
-    dropdown.classList.remove('hidden');
+navclickButton.addEventListener("click", () => {
+  // Hide navclickButton, Show navCloseButton
+  navclickButton.classList.add("hidden");
+  navCloseButton.classList.remove("hidden");
+  dropdown.classList.remove("hidden");
 });
 
-navCloseButton.addEventListener('click', () => {
-    // Toggle visibility of navclickButton and navCloseButton
-    navclickButton.classList.remove('hidden');
-    navCloseButton.classList.add('hidden');
+navCloseButton.addEventListener("click", () => {
+  // Toggle visibility of navclickButton and navCloseButton
+  navclickButton.classList.remove("hidden");
+  navCloseButton.classList.add("hidden");
 
-    // Hide the dropdown
-    dropdown.classList.add('hidden');
+  // Hide the dropdown
+  dropdown.classList.add("hidden");
 });
